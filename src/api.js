a59abf7144de7068617d057ec6a94264c5d7e49a
aruzh <aruzhan.kalieva.05@mail.ru>
2025-06-03 14:25:12 +0500

    render

--- src/api.js
+++ src/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://riskrate-356a.onrender.com/api",
+    baseURL: "https://49f1-213-211-113-148.ngrok-free.app/api",
 });
 
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem("token");
     if (token) req.headers.Authorization = `Bearer ${token}`;
+    // СНИМАЕМ warning
+    req.headers["ngrok-skip-browser-warning"] = "true";
     return req;
 });
 
